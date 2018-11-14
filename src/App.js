98f3bc3b967bf7c98629083b235de67fcdfb32b4
javier526 <twitch7894@gmail.com>
2018-11-14 13:45:45 -0300

    iniciando proyecto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Exercises from './exercises2';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Exercises />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/exercises2.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Exercises extends Component {
+    render() {
+        return (
+            <div className="Container">
+               <div className="item">1</div>
+               <div className="item">2</div>
+               <div className="item">3</div>
+               <div className="item">4</div>
+            </div>
+        )
+    }
+}
+export default Exercises
